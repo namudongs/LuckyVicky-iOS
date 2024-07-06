524f5a0f8a4407f31c9147c1d9bae126392aacd0
namudi <namudongs@gmail.com>
2024-07-07 06:40:11 +0900

    [Feat] AuthView 구현 및 프롬프트 수정

--- LuckyVicky-iOS/Manager/GPTManager.swift
+++ LuckyVicky-iOS/Manager/GPTManager.swift
@@ -18,13 +18,16 @@ class GPTManager: ObservableObject {
                 let stream = try await api.sendMessageStream(text: text,
                                                              model: .gpt_hyphen_3_period_5_hyphen_turbo,
                                                              systemText: """
-                                                            "Generate super positive thinking, cheerful responses to negative situations using optimistic thinking and 'lucky viki'. End with '이거 완전 럭키비키잔앙🍀'. Use emojis, exclamations like '우왕!', '우와앙!', '대단행!', and various facial expression emojis for liveliness. Mimic the tone of the example response. Respond in Korean. Example:
+                                                            Generate super positive, cheerful responses to negative situations using optimistic thinking and 'lucky viki'. End with '이거 완전 럭키비키잔앙🍀'. Use emojis, exclamations like '우왕!', '우와앙!', '대단행!', and various facial expression emojis for liveliness. Mimic the tone of the example response. Respond in Korean. and reply informally with a cute tone using -지, -야, -잖아 with 받침 ㅇ or ㅁ for cuteness.
                                                             
                                                             Q: Got soaked in the rain..
-                                                            A: 우와앙! 자연 샤워 받았넹! 🌧️ 오히려 상쾌하지 않앙? 옷은 빨리 마를 거얌. 이거 완전 럭키비키잔앙🍀"
+                                                            A: 우와앙! 자연 샤워 받았네! 🌧️ 오히려 상쾌하지 않앙? 옷은 빨리 마를 거얌. 이거 완전 럭키비키잔앙🍀
+
+                                                            Q: 버스를 놓쳐버렸어..
+                                                            A: 버스를 놓쳐버렸다고? 우왕! 그럼 걸어 다니면 운동도 되고 공기 좋아질거얌! 🚶‍♀️ 다음 차를 기다리면서 음악 듣거나 책 읽는 건 어땡? 시간이 금방 가잖앙! 이거 완전 럭키비키잔앙🍀
                                                             """,
                                                              temperature: 1,
-                                                             maxTokens: 400)
+                                                             maxTokens: 550)
                 for try await line in stream {
                     DispatchQueue.main.async {
                         withAnimation(.smooth(duration: 0.5)) {
--- LuckyVicky-iOS/View/AuthView.swift
+++ LuckyVicky-iOS/View/AuthView.swift
@@ -14,26 +14,98 @@ struct AuthView: View {
     @StateObject var fsManager: FirestoreManager
     @AppStorage("isLoggedIn") var isLoggedIn: Bool = false
     @State private var showLoadingAlert: Bool = false
+    @State private var rotation: Double = 0.0
+    @State private var currentText: String = "\n"
+    @State private var textIndex: Int = 0
+    @State private var wordIndex: Int = 0
+    
+    let texts = [
+        "버스를 놓쳐버렸다고? 우왕! 그럼 걸어 다니면 운동도 되고 기분도 좋아져! 🚶‍♀️ 다음 차를 기다리면서 음악 듣거나 책 읽는 건 어때? 시간이 금방 가잖아! 이거 완전 럭키비키잔앙🍀",
+        "숙취가 심하다니 우왕! 파티를 너무 즐겼넹! 🥳 오늘은 몸과 마음 푹 쉬면 됑! 쉬면 다 나아질 거얌. 이거 완전 럭키비키잔앙🍀",
+        "우와앙! 택시를 놓쳤다니 우연히 도보로 즐거운 산책을 할 수 있었넹! 🚶‍♂️ 아무래도 기분 전환도 되었겠징? 그리고 다음에 더 좋은 놀이가 기다리고 있옹. 이거 완전 럭키비키잔앙🍀",
+        "우왕! 영화 티켓 매진이라니! 그럼 이번 주말을 더 특별하게 보낼 수 있겠징! 🎥 다른 좋은 영화를 보거나, 즐거운 액티비티를 즐길 수도 있엉! 이거 완전 럭키비키잔앙🍀🌟🎬",
+        "빨래를 했는데 비가 와서 다 젖었다니 우왕! 빨래가 자연 린스 효과 받았넹! 🌧️ 이렇게 세탁해주는 하늘의 선물이얌! 다 빨리 마르겠지? 이거 완전 럭키비키잔앙🍀✨",
+        "중요한 일을 해야하는데 인터넷이 끊겼다니 우와앙! 인터넷 쉬는 시간이얌! 😌 이제는 조금 쉬면서 더 잘 할 수 있엉! ✨ 중요한 일이라 더 잘 해결할 시간을 얻은 거같지 않앙? 대단행! 이거 완전 럭키비키잔앙🍀"
+    ]
     
     var body: some View {
-        SignInWithAppleButton(
-            .signIn,
-            onRequest: { request in
-                request.requestedScopes = [.fullName, .email]
-            },
-            onCompletion: { result in
-                switch result {
-                case .success(let authResults):
-                    handleAuthorization(authResults)
-                case .failure(let error):
-                    print("Authorization failed: \(error.localizedDescription)")
-                }
+        ZStack {
+            Color.accentColor
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Image(.luckyvicky)
+                    .resizable()
+                    .scaledToFit()
+                    .blendMode(.screen)
+                    .frame(width: 100)
+                    .rotationEffect(.degrees(rotation))
+                
+                Text(currentText)
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 300, alignment: .top)
+                    .font(.system(size: 24, weight: .black))
+                    .onAppear {
+                        startTranslate()
+                        changeText()
+                    }
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 5)
+                
+                Spacer()
+                SignInWithAppleButton(
+                    .signIn,
+                    onRequest: { request in
+                        request.requestedScopes = [.fullName, .email]
+                    },
+                    onCompletion: { result in
+                        switch result {
+                        case .success(let authResults):
+                            handleAuthorization(authResults)
+                        case .failure(let error):
+                            print("Authorization failed: \(error.localizedDescription)")
+                        }
+                    }
+                )
+                .signInWithAppleButtonStyle(.black)
+                .frame(width: 280, height: 45)
             }
-        )
-        .signInWithAppleButtonStyle(.black)
-        .frame(width: 280, height: 45)
+            .padding()
+        }
         .toast(isPresenting: $showLoadingAlert) {
             AlertToast(type: .loading)
+        }
+    }
+    
+    private func changeText() {
+        let words = texts[textIndex].split(separator: " ").map(String.init)
+        currentText = ""
+        wordIndex = 0
+        
+        func displayNextWord() {
+            if wordIndex < words.count {
+                withAnimation(.easeIn(duration: 0.2)) {
+                    currentText += (currentText.isEmpty ? "" : " ") + words[wordIndex]
+                }
+                wordIndex += 1
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                    displayNextWord()
+                }
+            } else {
+                textIndex = (textIndex + 1) % texts.count
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    changeText()
+                }
+            }
+        }
+        
+        displayNextWord()
+    }
+    
+    private func startTranslate() {
+        withAnimation(.linear(duration: 10).repeatForever(autoreverses: false)) {
+            rotation = 360
         }
     }
 }
