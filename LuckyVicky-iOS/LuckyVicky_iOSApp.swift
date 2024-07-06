aa8131ea0047ef4a33c5b811740bc02569a5ba7f
namudi <namudongs@gmail.com>
2024-07-06 16:18:18 +0900

    [Setting] Project Init

---
+++ LuckyVicky-iOS/LuckyVicky_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuckyVicky_iOSApp.swift
+//  LuckyVicky-iOS
+//
+//  Created by namdghyun on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LuckyVicky_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
